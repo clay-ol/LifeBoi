cdfaac5f621f35679bf4a0558b12ffa6efaee0db
jetaehyun <jetaehyun0@gmail.com>
2020-10-06 14:49:28 -0400

    added buttons

--- app/src/main/java/com/bignerdranch/android/lifeboi/AppointmentActivity.kt
+++ app/src/main/java/com/bignerdranch/android/lifeboi/AppointmentActivity.kt
@@ -1,11 +1,23 @@
 package com.bignerdranch.android.lifeboi
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+private const val APPOINTMENT_DATE = "com.bignerdranch.android.lifeboi.appointment_date"
 
 class AppointmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_appointment)
+    }
+
+    companion object{
+        fun newIntent(packageContext: Context) : Intent {
+            return Intent(packageContext, HomeActivity::class.java).apply {
+                putExtra(APPOINTMENT_DATE, true)
+            }
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/lifeboi/HomeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/lifeboi/HomeActivity.kt
@@ -4,12 +4,34 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 const val EXTRA_USER_FOUND = "com.bignerdranch.android.lifeboi.user_found"
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var weatherButton: Button
+    private lateinit var eventButton: Button
+    private lateinit var stepsButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        weatherButton = findViewById(R.id.weather_button)
+        eventButton = findViewById(R.id.event_button)
+        stepsButton = findViewById(R.id.steps_button)
+
+        weatherButton.setOnClickListener {
+
+        }
+
+        eventButton.setOnClickListener {
+
+        }
+
+        stepsButton.setOnClickListener {
+
+        }
     }
 
     companion object{
