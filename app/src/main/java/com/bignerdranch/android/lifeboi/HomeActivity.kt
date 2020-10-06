5df3ad41b12564c0020a367e04ceca336421452a
jdang4 <jdang@wpi.edu>
2020-10-06 14:37:16 -0400

    allowed transition from Splash to Home

--- app/src/main/java/com/bignerdranch/android/lifeboi/HomeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/lifeboi/HomeActivity.kt
@@ -1,11 +1,22 @@
 package com.bignerdranch.android.lifeboi
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+const val EXTRA_USER_FOUND = "com.bignerdranch.android.lifeboi.user_found"
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    companion object{
+        fun newIntent(packageContext: Context) : Intent {
+            return Intent(packageContext, HomeActivity::class.java).apply {
+                putExtra(EXTRA_USER_FOUND, true)
+            }
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/lifeboi/SplashActivity.kt
+++ app/src/main/java/com/bignerdranch/android/lifeboi/SplashActivity.kt
@@ -2,10 +2,31 @@ package com.bignerdranch.android.lifeboi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+private const val REQUEST_HOME_SCREEN = 0
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var loginButton: Button
+    private lateinit var signUpButton: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton = findViewById(R.id.login_button)
+        signUpButton = findViewById(R.id.signup_button)
+
+        loginButton.setOnClickListener {
+            val intent = HomeActivity.newIntent(this@SplashActivity)
+            startActivityForResult(intent, REQUEST_HOME_SCREEN)
+        }
+
+        signUpButton.setOnClickListener {
+            val intent = HomeActivity.newIntent(this@SplashActivity)
+            startActivityForResult(intent, REQUEST_HOME_SCREEN)
+        }
     }
 }
